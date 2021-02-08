f86634b1980a68cb0d2ebd59c203defb9677c74c
Harshita-Git <harshitamohadikar4@gmail.com>
2021-02-08 19:41:38 +0530

    Initial commit

---
+++ app/src/main/java/com/harshita/motionsplashscreen/MainActivity.java
@@ -0,0 +1,32 @@
+package com.harshita.motionsplashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    Thread timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        timer = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    synchronized (this){
+                        wait(10000);
+                    }
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
